5d26ed8feb751f6f2abd498c1ebc11a720dffba2
Abbas <muhdabbas98@gmail.com>
2024-09-13 09:59:58 +0800

    :bug: Change word

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Uploader from "./uploader";
 export default function Home() {
   return (
     <div className='flex flex-col max-w-screen-md items-center min-h-dvh justify-center py-2 gap-6 px-10 text-center'>
-      <h1 className='font-brittany text-5xl text-onyx'>Share the moment!</h1>
+      <h1 className='font-brittany text-5xl text-onyx'>Share the moment</h1>
       <Image
         src='/letter-h-a.png'
         width={500}
